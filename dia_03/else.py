280a27a58fde4bf5cea4e286929606844dce46cb
Vanessa Liz <vanessalizsousa1996@gmail.com>
2025-11-11 22:01:55 -0300

    terceiro dia de prática

---
+++ dia_03/else.py
@@ -0,0 +1,12 @@
+# %%
+
+idade = 85
+
+if idade > 70:
+    print("Cuidado com a bebida. COnsulte o seu geriatra")
+
+if idade >= 18:
+    print("Você pode beber cerveja! Beba com moderação")
+
+else: 
+    print("Você não pode beber")
---
+++ dia_03/if.py
@@ -0,0 +1,8 @@
+# %%
+
+idade = 16
+
+if idade >= 18:
+    print("Você pode beber cerveja")
+if idade <= 17:
+    print("Você não pode beber cerveja")
